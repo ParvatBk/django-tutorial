0f984769691f03168398765d0353538521a4adb6
pb325 <pb325@student.le.ac.uk>
2021-11-12 12:47:59 +0000

    Modified view to use template and context for displaying all the polls.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    #creating template
+    template = loader.get_template('polls/index.html')
+    #creating context with all the questions in the poll
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #output = ', '.join([q.question_text for q in latest_question_list])
 
-    return HttpResponse(output)
+    #return context as HttpResponse
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
 def detail(request, question_id):
