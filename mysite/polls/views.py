861b37fbb7053dd893b0bf606a3df27cae5d46bc
pb325 <pb325@student.le.ac.uk>
2021-11-12 14:07:10 +0000

    Removed hardcoded urls in the index.html pages

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
-
+from django.http import Http404
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #creating template
@@ -20,7 +19,15 @@ def index(request):
 
 # Create your views here.
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist in this poll.")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question':question})
+    #return HttpResponse("You're looking at question %s." %question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
