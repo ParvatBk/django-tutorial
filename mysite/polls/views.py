9f52db9186dd606c2f581bcb5ca089e4a395c508
pb325 <pb325@student.le.ac.uk>
2021-11-12 23:42:12 +0000

    Added tests for IndexView, DetailView

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,18 +4,25 @@ from .models import Choice, Question
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView (generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
+        #only return questions whose pub_date is less than or equal to current time
+        #i.e. no future questions.
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
         #return last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView (generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView (generic.DetailView):
     model = Question
